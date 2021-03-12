78fb5a71cb997a25efca9feb6d7ed522fe3041e3
manucastilla <manuela.castilla@gmail.com>
2021-03-12 18:14:17 +0000

    respotas nao batem

---
+++ material/aulas/06-aleatorizacao/random.cpp
@@ -0,0 +1,11 @@
+#include <random>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    cout << distribution(generator);
+}
