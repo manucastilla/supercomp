37b18e9db81a9f9c96c30b3852b8de15a22f36dc
manucastilla <manuela.castilla@gmail.com>
2021-04-07 14:30:48 +0000

    relatorio feito

--- material/aulas/08-busca-global/tarefa2.cpp
+++ material/aulas/08-busca-global/tarefa2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 // N - num de objs
 // P - peso
-// valor dos objetos
+// v dos objetos
 // C capacidade
 
 /*  Pseudocódigo retornando o valor da mochila ótima
@@ -24,19 +24,23 @@ func(P, V, N, C, i)
     return semItem
 */
 
-int M(int N, vector<int> &P, vector<int> &V, int C, int i, vector<int> &used, vector<int> &best)
+int M(int N, vector<int> &P, vector<int> &V, int C, int i, vector<bool> &used, vector<bool> &best)
 {
-    int valorUsed = 0;
-    int valorBest = 0;
+
     if (i == N)
     {
-        if (used[i])
+        int valorUsed = 0;
+        int valorBest = 0;
+        for (int i = 0; i < N; i++)
         {
-            valorUsed += V[i];
-        }
-        if (best[i])
-        {
-            valorBest += V[i];
+            if (used[i])
+            {
+                valorUsed += V[i];
+            }
+            if (best[i])
+            {
+                valorBest += V[i];
+            }
         }
 
         if (valorUsed > valorBest)
@@ -45,18 +49,21 @@ int M(int N, vector<int> &P, vector<int> &V, int C, int i, vector<int> &used, ve
         }
         return 0;
     }
-    used[i] = 0;
+    // used[i] = 0;
+    used[i] = false;
     int semItem, comItem;
 
     semItem = M(N, P, V, C, i + 1, used, best);
-    comItem = semItem;
 
-    if (C - P[i] > 0)
+    if (C - P[i] >= 0)
     {
+        // used[i] = 1;
+        used[i] = true;
         comItem = M(N, P, V, C - P[i], i + 1, used, best) + V[i];
+        return max(semItem, comItem);
     }
 
-    return max(semItem, comItem);
+    return semItem;
 }
 
 int main()
@@ -66,20 +73,31 @@ int main()
     int C;
     std::cin >> C;
 
-    vector<int> peso(n);
-    vector<int> valor(n);
-    vector<int> used(n);
-    vector<int> best(n);
+    vector<int> P(n);
+    vector<int> v(n);
+    vector<bool> used(n, false);
+    vector<bool> best(n, false);
 
+    int wi, vi;
     for (int i = 0; i < n; i++)
     {
 
-        cin >> peso[i];
-        cin >> valor[i];
+        cin >> wi >> vi;
+        P[i] = wi;
+        v[i] = vi;
     }
 
-    cout << M(n, peso, valor, C, 0, used, best) << '\n';
+    int Value = M(n, P, v, C, 0, used, best);
 
+    int wTotal = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (best[i])
+        {
+            wTotal += P[i];
+        }
+    }
+    cout << wTotal << " " << Value << " " << '1' << "\n ";
     for (int i = 0; i < n; i++)
     {
         if (best[i])
